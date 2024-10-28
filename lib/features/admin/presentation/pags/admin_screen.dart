abdb076cdb6e450095a1fd0560653e3800926448
RobinSrk <robinsrk3@gmail.com>
2024-10-28 16:05:45 +0600

    updated admin page

--- lib/features/admin/presentation/pags/admin_screen.dart
+++ lib/features/admin/presentation/pags/admin_screen.dart
@@ -1,3 +1,4 @@
+import 'package:deshi_ponno/core/localization/app_localization.dart';
 import 'package:flutter/material.dart';
 
 class AdminScreen extends StatefulWidget {
@@ -10,9 +11,13 @@ class AdminScreen extends StatefulWidget {
 class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Under construction"),
+    return Scaffold(
+      appBar:
+          AppBar(title: Text(AppLocalizations.of(context).translate("admin"))),
+      body: const Column(
+        children: [
+          Text("Under construction"),
+        ],
       ),
     );
   }
